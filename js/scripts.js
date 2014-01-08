81cb1e7a32a3d67db4f6d496a071ace1811b4562
matthew <matthew@epiphanydesign.co>
2014-01-08 13:51:10 -0600

    Major overhaul

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,61 @@
 $(document).ready(function() {
     
-/*----- SCROLLING MENU -----*/
-
-    function fade_header() {
+ $('#venues-slider').superslides();
     
-		if ($(window).width() >= 720) {
+/*----- NAVIGATION -----*/
+    
+    if (!location.hash) {
+        $('nav ul li a:first').addClass('active');
+        $('nav ul li:first').addClass('liActive');
+    } else if ($('nav ul li a').hasClass('logotype')) {
+        $(this).parent().removeClass('liActive');
+    }    
+    else {
+        $('nav ul li a[href=' + location.hash + ']').addClass('active');
+        $('nav ul li a[href=' + location.hash + ']').parent().addClass('liActive');
+    }
 
-			winScroll = $(this).scrollTop();
-
-			if (winScroll > $('header').height() - 1) {
-
-				$('#navScroll').slideDown('slow');
-                
-			} else {
-				
-				$('#navScroll').fadeOut('fast');
-        
-			}
-            
-		} else if ($(window).width() >= 480 && $(this).width() <= 720) {
-            
-            $('.min').show();
-            
+    $('nav ul li a').bind('click', function(event) {
+        $('nav ul li a').removeClass('active');
+        $('nav ul li').removeClass('liActive');
+        $(this).addClass('active');
+        if ( $(window).width() > 850 ) {
+        $(this).parent().addClass('liActive');
         } else {
             
         }
-
-}
+    });
     
-    $(window).scroll(function() { fade_header() });
+/*----- SCROLLING MENU -----*/
 
+function fade_header() {
+
+		if ( $(window).width() > 300 ) {
+            
+			window_scroll = $(this).scrollTop();
+            
+            if ( window_scroll > ($('#home').height())) {
+
+				$('header').css({"height": 40});
+                $('header nav ul li a').css({
+                    "line-height": '40px'
+                });
+			} 
+            else {
+				
+				$('header').css({"height": 60});
+                $('header nav ul li a').css({
+                    "line-height": '60px'
+                });            
+			}
+		}
+	}
+    $(window).scroll(function() { fade_header(); 
+                                
+});
+    
+    fade_header();
+    
 /*----- RESPONSIVENESS -----*/    
     
     function mobileStyling() {
@@ -53,17 +79,14 @@ $(document).ready(function() {
     } else {}
         
     });
-        
-        
-        
-
-/*----- COLOR SCROLLING HEADER -----*/
+    
+    /*----- COLOR SCROLLING HEADER -----*/
 
     var scroll_pos = 0;
     var animation_begin_pos = 0; //where you want the animation to begin
     var animation_end_pos = 1000; //where you want the animation to stop
-    var beginning_color = new $.Color( 'rgba(0,161,75,0.3)' ); //we can set this here, but it'd probably be better to get it from the CSS; for the example we're setting it here.
-    var ending_color = new $.Color( 'rgba(0,161,75,0.99)' ); ;//what color we want to use in the end
+    var beginning_color = new $.Color( 'rgba(32,175,100,0.6)' ); //we can set this here, but it'd probably be better to get it from the CSS; for the example we're setting it here.
+    var ending_color = new $.Color( 'rgba(32,175,100,0.99)' ); ;//what color we want to use in the end
     $(document).scroll(function() {
         scroll_pos = $(this).scrollTop(); 
         if(scroll_pos >= animation_begin_pos && scroll_pos <= animation_end_pos ) { 
@@ -76,94 +99,12 @@ $(document).ready(function() {
             var newAlpha = beginning_color.alpha() + ( ( ending_color.alpha() - beginning_color.alpha() ) * percentScrolled );
             var newColor = new $.Color( newRed, newGreen, newBlue, newAlpha );
             //console.log( newColor.red(), newColor.green(), newColor.blue() );
-            $('header').animate({ backgroundColor: newColor }, 0);
+            $('.overlay').animate({ backgroundColor: newColor }, 0);
         } else if ( scroll_pos > animation_end_pos ) {
-             $('header').animate({ backgroundColor: ending_color }, 0);
+             $('.overlay').animate({ backgroundColor: ending_color }, 0);
         } else if ( scroll_pos < animation_begin_pos ) {
-             $('header').animate({ backgroundColor: beginning_color }, 0);
+             $('.overlay').animate({ backgroundColor: beginning_color }, 0);
         } else { }
-    });    
-
-/*----- STAFF SECTION -----*/
-    
-    var document = $(document);
-    
-$('#mesaros, #keeler, #watkins').click(function(event){
-    // animate staff bio slide down
-    var staffID = $(this).attr('id');
-    console.log(yCoord);
-    
-        $(this).animate({
-            top: 20
-        }, 1000, function(){
-
-        });
-    
-    var yCoord = $(this).position.top;
-
-    if (yCoord > 0) {
-        $(this).animate({
-            top: -20
-        })
-    } else {
-
-    }
+    }); 
         
-    if (staffID == 'mesaros') {
-        $('#mesarosBio').slideToggle();
-        $(this).prev('div').animate({
-            opacity: 0.4
-        }, 500, 'linear');
-            
-        $(this).next('div').animate({
-            opacity: 0.4
-        }, 500, 'linear');
-        staffID = 0;
-    } else if (staffID == 'keeler') {
-        $('#keelerBio').slideToggle();
-    } else if (staffID == 'watkins') {
-        $('#watkinsBio').slideToggle();
-    } else {
-        
-    }
-
-    // hide all other staff members not currently selected
-    if (staffID == 0) {
-        
-    $(this).siblings('div').animate({
-        opacity: 1
-    }, 500, 'linear');
-        
-    } else {
-        
-    }
-
-    return false;
-});
-    
-
-
-/*
-$('.staffPhoto').hover(function() {
-    var photoWidth = parseInt($(this).css('width'),10);
-    if (photoWidth < 170) {
-        $(this).animate({
-            height: 170,
-            width: 170
-        }, 200, 'swing', function(){
-           
-        });
-    } else {
-        $(this).animate({
-            height: 150,
-            width: 150
-        }, 200, 'swing', function(){
-           
-        }); 
-    }
-    
-});
-
-*/
-    
 });
